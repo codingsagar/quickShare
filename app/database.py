aac223067ea2ba6cd8b6513fc460138365b498df
“codingsagar” <68506265+codingsagar@users.noreply.github.com>
2024-10-28 18:17:42 +0530

    Added the download file route and fix the file extension thing

--- app/database.py
+++ app/database.py
@@ -1,13 +1,28 @@
-from sqlmodel import SQLModel
-from sqlalchemy.ext.asyncio import create_async_engine
-import asyncio
+from sqlmodel import Field, SQLModel, create_engine,Session
+import os
+from datetime import datetime
+from typing import Optional,Generator
 
-DATABASE_URL = "sqlite:///./database.db"  
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+class File(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    key: str = Field(index=True, nullable=False)  
+    url: str = Field(nullable=False) 
+    uploaded_at: datetime = Field(default_factory=datetime.now) 
+    downloaded: bool = Field(default=False) 
 
-async def create_db_and_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
 
-asyncio.run(create_db_and_tables())
+DATABASE_URL = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOSTNAME')}:{os.getenv('DB_PORT')}/quickshare"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+def db_init():
+    try:
+        SQLModel.metadata.create_all(engine)
+        print("Tables created successfully!")
+    except Exception as e:
+        print(f"Error creating tables: {e}")
+
+def get_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session 
--- app/main.py
+++ app/main.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI,UploadFile, HTTPException
+from fastapi import FastAPI,UploadFile, HTTPException,Depends
+from fastapi.responses import RedirectResponse
 import boto3
+from sqlmodel import Session,select
 from botocore.exceptions import NoCredentialsError
 import os
 from dotenv import load_dotenv
+from database import db_init,File,get_session
+from contextlib import asynccontextmanager
+from uuid import uuid4
+from datetime import datetime,timedelta
 
 load_dotenv()
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    db_init()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 s3_client = boto3.client(
@@ -18,26 +31,77 @@ s3_client = boto3.client(
 
 
 
+
 @app.get("/")
 def read_root():
     return {"success": True}
 
 
 @app.post("/files/upload/")
-async def upload_file(file: UploadFile):
+async def upload_file(file: UploadFile,
+session: Session = Depends(get_session)):
     try:
         file_content = await file.read()
 
+        extension = file.filename.split('.')[-1]
+
+        unique_filename = f"{str(uuid4())}.{extension}"
+
         s3_client.put_object(
             Bucket=os.getenv("S3_BUCKET_NAME"),
-            Key=file.filename,
+            Key= unique_filename,
             Body=file_content,
             ContentType=file.content_type
         )
 
-        return {"message": f"File '{file.filename}' uploaded successfully to S3."}
+        presigned_url : str = s3_client.generate_presigned_url(
+            ClientMethod='get_object',
+            Params={
+                'Bucket': f'{os.getenv("S3_BUCKET_NAME")}',
+                'Key': f'{unique_filename}',
+                'ResponseContentDisposition': f'attachment;filename={file.filename}'
+            },
+            ExpiresIn=630
+        )
+
+        download_id = str(uuid4())
+
+        new_file = File(
+            key= download_id,
+            url= f"{presigned_url}",
+            downloaded= False,
+        )
+
+        session.add(new_file)
+        session.commit()
+        session.refresh(new_file)
+
+        return {"message": f"File '{file.filename}' uploaded successfully to S3.","url":f"http://127.0.0.1:8000/download/{download_id}"}
 
     except NoCredentialsError:
         raise HTTPException(status_code=403, detail="AWS credentials are missing or incorrect.")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/download/{download_id}/")
+async def download_file(download_id: str, db: Session = Depends(get_session)):
+
+    statement = select(File).where(File.key == download_id)
+
+    file_record = db.exec(statement).first()
+
+    if not file_record:
+        raise HTTPException(status_code=404, detail="File not found")
+    
+
+    if datetime.now() > file_record.uploaded_at + timedelta(minutes=10.5):
+        raise HTTPException(status_code=410, detail="File has expired")
+
+    if file_record.downloaded:
+        raise HTTPException(status_code=403, detail="File already downloaded")
+
+    file_record.downloaded = True
+    db.commit()
+
+    return RedirectResponse(url=file_record.url)
